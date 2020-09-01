86ee7c5d62fce4e52a816fd499509b1e7b009877
time2w8 <emgore.1410@gmail.com>
2020-09-01 01:47:35 -0500

    Update bussiness list endpoint

--- src/api/bussiness/bussiness.controller.ts
+++ src/api/bussiness/bussiness.controller.ts
@@ -4,7 +4,8 @@ import { BussinessService } from './bussiness.service';
 @Controller('bussiness')
 export class BussinessController {
     constructor(private readonly _bussinessService: BussinessService) { }
-    @Get('')
+
+    @Get('/list')
     async getBussiness() {
         const bussiness = await this._bussinessService.getBussiness();
         return bussiness;
