096b2369ef11e46e3a5a984b62ebf6aa9c141ce3
time2w8 <emgore.1410@gmail.com>
2020-09-05 01:05:04 -0500

    Add to future API DOC SWAGGER

--- src/utils/constants/api.ts
+++ src/utils/constants/api.ts
@@ -11,6 +11,21 @@ export const api = { //For Api Documentation
         tag: 'user',
         description: 'Users REST API Documentation',
       },
+      {
+        name: 'Bussiness Example',
+        tag: 'bussiness',
+        description: 'Bussiness REST API Documentation',
+      },
+      {
+        name: 'Order Example',
+        tag: 'order',
+        description: 'Orders REST API Documentation',
+      },
+      {
+        name: 'Equipment Example',
+        tag: 'equipment',
+        description: 'Equipments REST API Documentation',
+      },
     ],
     version: '1.0',
   },
--- src/utils/constants/modules.ts
+++ src/utils/constants/modules.ts
@@ -1,7 +1,13 @@
 import { AuthModule } from '../../auth/auth.module';
 import { UserModule } from '../../api/user/user.module';
+import { BussinessModule } from 'src/api/bussiness/bussiness.module';
+import { OrderModule } from 'src/api/order/order.module';
+import { EquipmentModule } from 'src/api/equipment/equipment.module';
 
 export const modules = { //For API Documentation
-  user: UserModule,
   auth: AuthModule,
+  user: UserModule,
+  bussiness: BussinessModule,
+  order: OrderModule,
+  equipment: EquipmentModule
 };
