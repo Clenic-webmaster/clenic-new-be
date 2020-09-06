1b2eb5ef517d1f82af2d8f8eb43cf87534230cd2
time2w8 <emgore.1410@gmail.com>
2020-09-05 21:39:10 -0500

    Import role service in role middleware

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -1,9 +1,13 @@
-import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException, HttpStatus } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { JWTPayloadDto } from 'src/models/dto';
+import { RoleService } from 'src/api/role/role.service';
+import { Role } from 'src/models/interfaces';
+import { ErrorHandler } from 'src/utils/errors';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-    constructor(private reflector: Reflector) { }
+    constructor(private reflector: Reflector, private _roleService: RoleService) { }
 
     canActivate(context: ExecutionContext): boolean {
         const roles = this.reflector.get<string[]>('roles', context.getHandler());
