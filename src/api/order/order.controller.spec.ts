1025768b7ea2d52a627f501b996b2f783c77f557
ernesto_sanchez_matos <ernesto.sm.95@hotmail.com>
2020-11-07 04:24:17 -0500

    testing_

---
+++ src/api/order/order.controller.spec.ts
@@ -0,0 +1,9 @@
+describe('GET: Listar todas las ordenes',()=>{
+    //getOrders
+    test('Debe retornar un arreglo de objetos Order', ()=>{
+    const objBussiness=null;//=bussinessService.getBussinessById("5f5060342e858d4afcedece5");
+    var expected=objBussiness==null?false : true;
+    const respuesta=false;
+    expect(respuesta).toStrictEqual(expected);
+    })      
+})
