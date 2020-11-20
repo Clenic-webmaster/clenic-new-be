97c29cadffdf2c0d6adbb97e5f61bf2dbd223167
Adrian Crespo Ames <Adrian_Ames@outlook.com>
2020-11-19 23:44:00 -0500

    Role module modification

--- src/api/role/role.module.ts
+++ src/api/role/role.module.ts
@@ -4,8 +4,6 @@ import { RoleController } from './role.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { database } from 'src/utils/constants/database';
 
-// new endopoint added
-
 @Module({
     providers: [RoleService],
     exports: [RoleService],
