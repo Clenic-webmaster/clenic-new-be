63118a280bc563bfa56b6f9a7709510cb82c11a5
ernesto_sanchez_matos <ernesto.sm.95@hotmail.com>
2020-11-20 22:00:41 -0500

    corr_serv

--- src/api/bussiness/bussiness.service.spec.ts
+++ src/api/bussiness/bussiness.service.spec.ts
@@ -3,14 +3,14 @@ import {BussinessService} from "./bussiness.service";
 describe('Obtener detalle de negocio mediante su Id',()=>{
     //getBussinessById
     test('Debe retornar un objeto Bussiness si el Id existe', ()=>{
-    const objBussiness=null;//=bussinessService.getBussinessById("5f5060342e858d4afcedece5");
+    const objBussiness=null;
     var expected=objBussiness==null?false : true;
     const respuesta=false;
     expect(respuesta).toStrictEqual(expected);
     })
     
     test('Debe retornar NULL si el Id no es valido', ()=>{
-        const objBussiness=null;//=bussinessService.getBussinessById("5f5060342e858d4afcedece5");
+        const objBussiness=null;
         var expected=objBussiness==null?false : true;
         const respuesta=false;
         expect(respuesta).toStrictEqual(expected);
@@ -20,7 +20,7 @@ describe('Obtener detalle de negocio mediante su Id',()=>{
 describe('Obtener las ordenes generadas por un negocio',()=>{
     //getBussinessById
     test('Debe retornar un arreglo de objetos Order', ()=>{
-    const objBussiness=null;//=bussinessService.getBussinessById("5f5060342e858d4afcedece5");
+    const objBussiness=null;
     var expected=objBussiness==null?false : true;
     const respuesta=false;
     expect(respuesta).toStrictEqual(expected);
@@ -30,14 +30,14 @@ describe('Obtener las ordenes generadas por un negocio',()=>{
 describe('Borrar equipo',()=>{
     //deleteEquipment
     test('Debe retornar el string: El equipo médico ha sido eliminado con éxito', ()=>{
-    const objBussiness=null;//=bussinessService.getBussinessById("5f5060342e858d4afcedece5");
+    const objBussiness=null;
     var expected=objBussiness==null?false : true;
     const respuesta=false;
     expect(respuesta).toStrictEqual(expected);
     })  
 
     test('Debe retornar un objeto HttpException si no se encontro al equipo médico', ()=>{
-        const objBussiness=null;//=bussinessService.getBussinessById("5f5060342e858d4afcedece5");
+        const objBussiness=null;
         var expected=objBussiness==null?false : true;
         const respuesta=false;
         expect(respuesta).toStrictEqual(expected);
