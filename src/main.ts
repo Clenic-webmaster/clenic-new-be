0e2abd1b683e3bb1022c5488352ee82d05fcea64
time2w8 <emgore.1410@gmail.com>
2020-11-21 04:56:25 -0500

    Allow al origins in cors for development

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,19 @@ import * as bodyParser from 'body-parser';
 import * as helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.use(bodyParser.json({ limit: '120mb' }));
   app.use(bodyParser.urlencoded({ limit: '120mb', extended: true }));
   app.use(helmet());
+  var whitelist = ['https://localhost:4200', ''];
+  app.enableCors({
+    origin: function (origin, callback) {
+      callback(null, true)
+    },
+    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({
     validationError: {
       target: false,
