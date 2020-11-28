238a4c481ffd19229d26b9ecc0ab785ae2d9f3ce
time2w8 <emgore.1410@gmail.com>
2020-11-28 14:47:00 -0500

    Fix backend port

--- src/main.ts
+++ src/main.ts
@@ -31,6 +31,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3001);
+  await app.listen(3000);
 }
 bootstrap();
